ee15b022ba3adf5ad613a68d01091fa277b0689f
장유정 <306406j@gmail.com>
2023-11-11 18:18:56 +0900

    ClassStart5 For Commit

--- src/intermediate/class1/ClassStart5.java
+++ src/intermediate/class1/ClassStart5.java
@@ -16,8 +16,17 @@ public class ClassStart5 {
         Student[] students = new Student[]{student1, student2};
         // Student[] students = {student1, student2}; 이렇게 해줘도 됨
 
-        for (int i = 0; i < students.length; i++) {
-            System.out.println("이름 : "  + students[i].name + ", 나이 : " + students[i].age + ", 성적 : " + students[i].grade);
+//        for (int i = 0; i < students.length; i++) {
+//            System.out.println("이름 : "  + students[i].name + ", 나이 : " + students[i].age + ", 성적 : " + students[i].grade);
+//        }
+
+//        for (int i = 0; i < students.length; i++) {
+//            Student s = students[i];
+//            System.out.println("이름 : "  + s.name + ", 나이 : " + s.age + ", 성적 : " + s.grade);
+//        }
+
+        for (Student s : students) {
+            System.out.println("이름 : "  + s.name + ", 나이 : " + s.age + ", 성적 : " + s.grade);
         }
     }
 }
