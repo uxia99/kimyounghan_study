3c0c4becd9aa2103791511e14319cf7920e52ffd
장유정 <306406j@gmail.com>
2023-11-12 18:12:23 +0900

    ClassStart5 Commit'

---
+++ src/intermediate/class1/ClassStart5.java
@@ -0,0 +1,23 @@
+package intermediate.class1;
+
+public class ClassStart5 {
+
+    public static void main(String[] args) {
+        Student student1 = new Student();
+        student1.name = "학생1";
+        student1.age = 15;
+        student1.grade = 90;
+
+        Student student2 = new Student();
+        student2.name = "학생2";
+        student2.age = 16;
+        student2.grade = 80;
+
+        Student[] students = new Student[]{student1, student2};
+        // Student[] students = {student1, student2}; 이렇게 해줘도 됨
+
+        for (int i = 0; i < students.length; i++) {
+            System.out.println("이름 : "  + students[i].name + ", 나이 : " + students[i].age + ", 성적 : " + students[i].grade);
+        }
+    }
+}
