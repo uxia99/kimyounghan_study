18bfa9f05eb56f4ad2de11449f7f88ca841660b6
장유정 <306406j@gmail.com>
2023-12-24 17:14:08 +0900

    MethodValue2 Commit

--- src/method/MethodValue2.java
+++ src/method/MethodValue2.java
@@ -3,15 +3,15 @@ package method;
 public class MethodValue2 {
 
     public static void main(String[] args) {
-        int num1 = 5;
-        System.out.println("1. changeNumber 호출 전, num1 = " + num1); // 5
-        changeNumber(num1);
-        System.out.println("4. changeNumber 호출 후, num1 = " + num1); // 5 아래 메서드에서 num2에 10을 넣었기 때문에 num1은 변하지 않음
+        int number = 5;
+        System.out.println("1. changeNumber 호출 전, number = " + number); // 5
+        changeNumber(number);
+        System.out.println("4. changeNumber 호출 후, number = " + number); // 5 아래 메서드에서 num2에 10을 넣었기 때문에 num1은 변하지 않음
     }
 
-    public static void changeNumber(int num2) {
-        System.out.println("2. changeNumber 변경 전, num2 = " + num2); // 5
-        num2 = num2 * 2;
-        System.out.println("3. changeNumber 변경 후, num2 = " + num2); // 10
+    public static void changeNumber(int number) {
+        System.out.println("2. changeNumber 변경 전, number = " + number); // 5
+        number = number * 2;
+        System.out.println("3. changeNumber 변경 후, number = " + number); // 10
     }
 }
