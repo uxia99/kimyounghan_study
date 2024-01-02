204fd5b8e67a2943d859c9f70c73f7eb5891cddb
장유정 <306406j@gmail.com>
2024-01-02 20:52:44 +0900

    ArrayQuiz4 Commit

---
+++ src/array/ArrayQuiz4.java
@@ -0,0 +1,30 @@
+package array;
+
+import java.util.Scanner;
+
+public class ArrayQuiz4 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("5개의 정수를 입력하시오 : ");
+        int[] arr = new int[5];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        System.out.print("입력한 정수의 합계 : ");
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        System.out.print(sum);
+        System.out.println();
+
+        System.out.print("입력한 정수의 합계 : ");
+        int average = sum / 5;
+        System.out.print(average);
+        System.out.println();
+    }
+}
