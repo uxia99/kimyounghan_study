393b377809fdb23763060eca24121194fd64200d
장유정 <306406j@gmail.com>
2024-01-03 20:45:06 +0900

    ArrayQuiz3 Commit

---
+++ src/array/ArrayQuiz3.java
@@ -0,0 +1,25 @@
+package array;
+
+import java.util.Scanner;
+
+public class ArrayQuiz3 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("5개의 정수를 입력하시오 : ");
+        int[] arr = new int[5];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        System.out.println("출력");
+        for (int i = 4; i >= 0; i--) {
+            System.out.print(arr[i]);
+            if (i > 0) {
+                System.out.print(", ");
+            }
+        }
+    }
+}
