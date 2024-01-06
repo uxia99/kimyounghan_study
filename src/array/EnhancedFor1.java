bd4cd62afffb8b10bf553c835c8b1ebe20c1f642
장유정 <306406j@gmail.com>
2024-01-06 18:23:13 +0900

    EnhancedFor1 Sec Commit

--- src/array/EnhancedFor1.java
+++ src/array/EnhancedFor1.java
@@ -12,10 +12,15 @@ public class EnhancedFor1 {
         }
         System.out.println();
 
-        // 향상된 for문 (for-each문) - 실무에서 가장 많이 사용
+        // 향상된 for문 (for-each문) - 실무에서 가장 많이 사용 - 단축키 iter
         for (int number : numbers) {
             System.out.print(number + " ");
         }
         System.out.println();
+
+        // for-each문을 사용할 수 없는 경우 - 증가하는 index 값 필요
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("number " + i + "번의 결과는 " + numbers[i] + "입니다.");
+        }
     }
 }
