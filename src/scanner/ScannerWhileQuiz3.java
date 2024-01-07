f8f55e5b1730d1b86833ae0768012fae969b50c6
장유정 <306406j@gmail.com>
2024-01-07 21:42:34 +0900

    ScannerWhileQuiz3 Commit

---
+++ src/scanner/ScannerWhileQuiz3.java
@@ -0,0 +1,11 @@
+package scanner;
+
+import java.util.Scanner;
+
+public class ScannerWhileQuiz3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
