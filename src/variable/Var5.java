983ef292c8778437b6770a732b7f05676fe6e167
장유정 <306406j@gmail.com>
2024-01-16 15:29:27 +0900

    variable

---
+++ src/variable/Var5.java
@@ -0,0 +1,19 @@
+package variable;
+
+public class Var5 {
+    public static void main(String[] args) {
+        // 변수 선언 및 초기화 별도로
+        int a;
+        a = 1;
+        System.out.println(a);
+
+        // 변수 선언과 초기화를 한번에
+        int b = 2;
+        System.out.println(b);
+
+        // 여러 변수 선언과 초기화를 한번에
+        int c = 3, d = 4;
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
