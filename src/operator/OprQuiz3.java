cfff5055c4918af98b4765da3d6044665ec5d642
장유정 <306406j@gmail.com>
2024-01-18 02:13:40 +0900

    OprQuiz3 Commit

---
+++ src/operator/OprQuiz3.java
@@ -0,0 +1,10 @@
+package operator;
+
+public class OprQuiz3 {
+    public static void main(String[] args) {
+        int score = 90;
+
+        boolean result = 80 <= score && score <= 100;
+        System.out.println(result); // true
+    }
+}
