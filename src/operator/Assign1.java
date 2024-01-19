4f72de8208cbf81099aacc03b98e69c67db0bf5e
장유정 <306406j@gmail.com>
2024-01-20 02:05:41 +0900

    Assign1 Commit

--- src/operator/Assign1.java
+++ src/operator/Assign1.java
@@ -1,4 +1,22 @@
 package operator;
 
 public class Assign1 {
+    public static void main(String[] args) {
+        int a = 5;
+        a += 3;
+
+        System.out.println(a); // 8
+
+        a -= 2;
+        System.out.println(a); // 6
+
+        a *= 4;
+        System.out.println(a); // 24
+
+        a /= 3;
+        System.out.println(a); // 8
+
+        a %= 5;
+        System.out.println(a); // 3
+    }
 }
