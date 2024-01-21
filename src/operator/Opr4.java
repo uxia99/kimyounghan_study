dd0a7e7e946ad3ba467d59c1d5525c868ee374a9
장유정 <306406j@gmail.com>
2024-01-21 18:56:06 +0900

    Opr4 Commit

---
+++ src/operator/Opr4.java
@@ -0,0 +1,12 @@
+package operator;
+
+public class Opr4 {
+    public static void main(String[] args) {
+        int sum3 = 2 * 2 + 3 * 3;
+        int sum4 = (2 * 2) + (3 * 3);
+
+        // soutv 사용
+        System.out.println("sum3 = " + sum3); // 13
+        System.out.println("sum4 = " + sum4); // 13
+    }
+}
