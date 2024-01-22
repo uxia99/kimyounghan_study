10ca6df7c5b5b6dc0956b7923799cdc5c8c179f0
장유정 <306406j@gmail.com>
2024-01-23 02:02:27 +0900

    Logical2 Commit

---
+++ src/operator/Logical2.java
@@ -0,0 +1,13 @@
+package operator;
+
+public class Logical2 {
+    public static void main(String[] args) {
+        int a = 15;
+
+        // a는 10보다 크고 20보다 작다
+        boolean result1 = a > 10 && a < 20; // (a > 10) && (a < 20)
+        boolean result2 = 10 < a && a < 20;
+        System.out.println("Result = " + result1); // Result = true
+        System.out.println("Result = " + result2); // Result = true
+    }
+}
