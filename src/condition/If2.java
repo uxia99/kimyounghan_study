6ef45994712a0fe227bbeb81a4cc8258ddb14ddd
장유정 <306406j@gmail.com>
2024-01-25 03:05:47 +0900

    If2 Commit

---
+++ src/condition/If2.java
@@ -0,0 +1,13 @@
+package condition;
+
+public class If2 {
+    public static void main(String[] args) {
+        int age = 20;
+
+        if (age >= 20) {
+            System.out.println("성인 입니다.");
+        } else {
+            System.out.println("미성년자 입니다.");
+        }
+    }
+}
