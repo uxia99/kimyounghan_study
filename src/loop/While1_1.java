ab1ca514e2a4db315d8a7a91e577b97c69568934
장유정 <306406j@gmail.com>
2024-01-26 16:28:22 +0900

    While1_1 Commit

---
+++ src/loop/While1_1.java
@@ -0,0 +1,16 @@
+package loop;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class While1_1 {
+    public static void main(String[] args) {
+        int count = 0;
+
+        count++;
+        System.out.println("현재 숫자는 : " + count); // 1
+        count++;
+        System.out.println("현재 숫자는 : " + count); // 2
+        count++;
+        System.out.println("현재 숫자는 : " + count); // 3
+    }
+}
