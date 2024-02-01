9cba2fe6f8ac808eca0bac319fd8f0cf38b5c6fc
장유정 <306406j@gmail.com>
2024-02-01 21:13:20 +0900

    ScannerQuiz5 Commit

---
+++ src/scanner/ScannerQuiz5.java
@@ -0,0 +1,32 @@
+package scanner;
+
+import java.util.Scanner;
+
+public class ScannerQuiz5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("첫 번째 숫자를 입력하시오 : ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("두 번째 숫자를 입력하시오 : ");
+        int num2 = scanner.nextInt();
+
+        if (num1 > num2) {
+            int temp = num1;
+            num1 = num2;
+            num2 = temp;
+        }
+
+        System.out.println("두 정수 사이의 모든 정수");
+
+        for (int i = num1; i <= num2; i++) {
+
+            System.out.print(i);
+
+            if (i != num2) {
+                System.out.print(", ");
+            }
+        }
+    }
+}
