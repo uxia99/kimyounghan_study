f56fc11e846841eeedeca0648d9bdd217998e97b
장유정 <306406j@gmail.com>
2024-02-07 04:04:25 +0900

    WhileQuiz2 Commit

---
+++ src/loop/WhileQuiz2.java
@@ -0,0 +1,12 @@
+package loop;
+
+public class WhileQuiz2 {
+    public static void main(String[] args) {
+        int num = 2;
+
+        while (num <= 20) {
+            System.out.println(num);
+            num += 2;
+        }
+    }
+}
