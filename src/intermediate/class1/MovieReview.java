effd01ec02fe7b17e5194f2ef0b3d6bc7f44390d
장유정 <306406j@gmail.com>
2024-02-21 18:35:09 +0900

    MovieReview Commit

---
+++ src/intermediate/class1/MovieReview.java
@@ -0,0 +1,6 @@
+package intermediate.class1;
+
+public class MovieReview {
+    String title;
+    String review;
+}
---
+++ src/intermediate/class1/MovieReviewMain1.java
@@ -0,0 +1,20 @@
+package intermediate.class1;
+
+public class MovieReviewMain1 {
+
+    public static void main(String[] args) {
+        MovieReview movie1 = new MovieReview();
+        movie1.title = "인셉션";
+        movie1.review = "인생은 무한루프";
+
+        MovieReview movie2 = new MovieReview();
+        movie2.title = "어바웃타임";
+        movie2.review = "인생 시간 영화";
+
+        MovieReview[] movies = new MovieReview[] {movie1, movie2};
+
+        for (MovieReview m : movies) {
+            System.out.println("영화 제목 : " + m.title + ", 리뷰 : " + m.review);
+        }
+    }
+}
